fc3b33057238f58682ff39da047a0370801a294a
shadreza <45334134+shadreza@users.noreply.github.com>
2021-03-20 04:17:12 +0600

    header done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
+import Header from './Components/Header/Header';
+import HomePage from './Components/HomePage/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Header></Header>
+      <Switch>
+        <Route path='/homepage'>
+          <HomePage></HomePage>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Header.css';
+import { Link, Router } from 'react-router-dom';
+import { Button } from '@material-ui/core';
+
+const Header = () => {
+    return (
+        <div className="headerMainDiv">
+            <section className="headerTitleOnLeft">
+                <Link className="navLinks" to='/home'>
+                    <h2>Civic Shifter</h2>
+                </Link>
+            </section>
+            <section className="headerNavLinksOnRight">
+                <nav>
+                    <Link className="navLinks" to='/home' >Home</Link>
+                    <Link className="navLinks" to='/destination' >Destination</Link>
+                    <Link className="navLinks" to='/contact' >Contact</Link>
+                    <Link className="navLinks" to='/login' >
+                        {
+                            <Button variant="outlined" color="secondary">
+                                Login
+                            </Button>
+                        }
+                    </Link>
+                </nav>
+            </section>
+        </div>
+    );
+};
+
+export default Header;
