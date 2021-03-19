6f12e15855660eb116541c7bd722357116850d27
shadreza <45334134+shadreza@users.noreply.github.com>
2021-03-20 03:18:48 +0600

    Initialized with react and firebase

---
+++ src/Components/FireBaseConfig/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA23dcIL94d05PKG88-4XGQeZeHgI5Gxwo",
+    authDomain: "civic-shifter.firebaseapp.com",
+    projectId: "civic-shifter",
+    storageBucket: "civic-shifter.appspot.com",
+    messagingSenderId: "971237895220",
+    appId: "1:971237895220:web:a29fc1192326533bfbee61"
+  };
+
+export default firebaseConfig;
