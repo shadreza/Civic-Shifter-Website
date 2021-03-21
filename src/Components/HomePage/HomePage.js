cf73d16c91cc909d6c6ecdc05874ee2e76d387f8
shadreza <45334134+shadreza@users.noreply.github.com>
2021-03-21 16:07:22 +0600

    default transport was solved

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import './HomePage.css';
 import FakeDataOfTransportJson from '../FakeData/FakeDataForTransports.json';
 import CardForTransport from '../CardForTransport/CardForTransport';
+import { ContextForTransport } from '../../App';
 
 const HomePage = () => {
+    const transportInfoFromContext = useContext(ContextForTransport);
+    useEffect(() =>{
+        const defaultTransport = {
+            name:"not set" ,
+            photo: "not set" 
+        }
+        transportInfoFromContext[1](defaultTransport);
+    },[])
     return (
         <div className="homepageMainDiv">
             <section className="transportsGallerySection">
