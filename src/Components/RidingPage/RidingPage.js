a784d2bda8e978ea61d7bf96024259106dedeb89
shadreza <45334134+shadreza@users.noreply.github.com>
2021-03-21 13:03:03 +0600

    corner cases in the links are done now need to work on the private auth

--- src/Components/RidingPage/RidingPage.js
+++ src/Components/RidingPage/RidingPage.js
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState } from 'react';
 import { Redirect, useHistory } from 'react-router-dom';
-import { ContextForUser } from '../../App';
+import { ContextForTransport, ContextForUser } from '../../App';
 import Map from '../Map/Map';
 import SearchedResultInLeftPanel from '../SearchedResultInLeftPanel/SearchedResultInLeftPanel';
 import SearchingInLeftPanel from '../SearchingInLeftPanel/SearchingInLeftPanel';
@@ -14,9 +14,14 @@ const RidingPage = () => {
     let history = useHistory();
 
     const userInfoFromContext = useContext(ContextForUser);
+    const transportInfoFromContext = useContext(ContextForTransport);
     if(userInfoFromContext[0].isLoggedInOrNot === false){
         alert('Please Log In First');
         history.replace('/login/');
+    }
+    else if(transportInfoFromContext[0].name ==='not set' && transportInfoFromContext[0].photo ==='not set'){
+        alert('Please Select Transport Type First');
+        history.replace('/');
     }
 
     const [hasBeenSearchedValue , sethasBeenSearchedValue] = useState(false);
