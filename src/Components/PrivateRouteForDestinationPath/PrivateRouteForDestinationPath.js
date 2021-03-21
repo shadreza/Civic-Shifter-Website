c4424f79ba3837b85c27155c833e2be8d59737aa
shadreza <45334134+shadreza@users.noreply.github.com>
2021-03-21 15:41:38 +0600

    Without private route

---
+++ src/Components/PrivateRouteForDestinationPath/PrivateRouteForDestinationPath.js
@@ -0,0 +1,24 @@
+// import React from 'react';
+// import { Route } from 'react-router-dom';
+
+// const PrivateRouteForDestinationPath = () => {
+//     return (
+//         <Route
+//       {...rest}
+//       render={({ location }) =>
+//         auth.user ? (
+//           children
+//         ) : (
+//           <Redirect
+//             to={{
+//               pathname: "/login",
+//               state: { from: location }
+//             }}
+//           />
+//         )
+//       }
+//     />
+//     );
+// };
+
+// export default PrivateRouteForDestinationPath;
